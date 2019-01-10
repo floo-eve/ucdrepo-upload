c99547ffb63598e14886cbbedefab0966634c337
Eveline Meyer <floo@devzone.ch>
2019-01-10 21:54:56 +0100

    added initial ldap security

--- src/main/java/ch/fad/ucd/ucdrepoupload/configuration/SecurityConfiguration.java
+++ src/main/java/ch/fad/ucd/ucdrepoupload/configuration/SecurityConfiguration.java
@@ -2,30 +2,46 @@ package ch.fad.ucd.ucdrepoupload.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    // http.authorizeRequests().antMatchers("home", "/resources/css/**",
+    // "/resources/fonts/**", "/resources/images/**")
+    // .permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
+    // .logout().permitAll();
+    // }
+
+    // @Bean
+    // @Override
+    // public UserDetailsService userDetailsService() {
+    // UserDetails user =
+    // User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
+    // .build();
+
+    // return new InMemoryUserDetailsManager(user);
+    // }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("home", "/resources/css/**", "/resources/fonts/**", "/resources/images/**")
-                .permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
-                .logout().permitAll();
+        http.authorizeRequests().anyRequest().fullyAuthenticated().and().formLogin();
     }
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication().userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org").and().passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder()).passwordAttribute("userPassword");
     }
 }
