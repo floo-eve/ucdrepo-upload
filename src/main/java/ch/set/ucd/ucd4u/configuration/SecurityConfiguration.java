5b783f9bfdcf90727ad5c6a719e4f0994187ecf2
Eveline Meyer <floo@devzone.ch>
2019-11-13 23:11:04 +0100

    changed to boostrap 4

--- src/main/java/ch/set/ucd/ucd4u/configuration/SecurityConfiguration.java
+++ src/main/java/ch/set/ucd/ucd4u/configuration/SecurityConfiguration.java
@@ -77,7 +77,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // this method allows static resources to be neglected by spring security
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**", "/static/**", "/webjars/**");
+        web.ignoring().antMatchers("/css/**", "/resources/**", "/static/**", "/webjars/**");
     }
 
 }
